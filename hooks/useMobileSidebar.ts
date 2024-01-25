1ec81b73e54060a92edb522d7ce3cc0a433f2582
Othamae <vero.gconesa@gmail.com>
2024-01-25 12:52:46 +0100

    feat: :sparkles: Created useMobileSidebarStore

---
+++ hooks/useMobileSidebar.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand"
+
+type MobileSidebarStore = {
+    isOpen: boolean
+    onOpen: () => void
+    onClose: () => void
+
+}
+
+export const useMobileSidebarStore = create<MobileSidebarStore>((set) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false })
+}))
