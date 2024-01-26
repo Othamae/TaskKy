a201d0d1239e015518603101bb32e4a6afceac02
Othamae <vero.gconesa@gmail.com>
2024-01-26 20:37:12 +0100

    feat: :sparkles: Added OrgInfo component in OrganizationIdPage

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,17 @@
-import { auth } from '@clerk/nextjs'
+import { Separator } from "@/components/ui/separator"
+import BoardList from "./_components/BoardList"
+import OrgInfo from "./_components/OrgInfo"
 
-const OrganizationIdPage = () => {
-	const { orgId } = auth()
-	return <span>Organization: {orgId}</span>
+const OrganizationIdPage = async () => {
+	return (
+		<div className='w-full mb-20'>
+			<OrgInfo />
+			<Separator className="my-4" />
+			<div className="px-2 md:px-4">
+				<BoardList />
+			</div>
+		</div>
+	)
 }
 
 export default OrganizationIdPage
