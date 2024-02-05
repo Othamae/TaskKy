c7a81af41b731c9cd0f4fbaa155ae6edacf9d149
Othamae <vero.gconesa@gmail.com>
2024-02-05 12:55:52 +0100

    feat: :sparkles: Created deleteBoard Actions

---
+++ actions/deleteBoard/index.ts
@@ -0,0 +1,39 @@
+'use server'
+import { db } from '@/lib/db'
+
+import { auth } from '@clerk/nextjs'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createSafeAction } from '../createSafeAction'
+import { DeleteBoard } from './schema'
+import { InputType, ReturnType } from './types'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+    const { userId, orgId } = auth()
+    if (!userId || !orgId) {
+        return {
+            error: 'Unauthorized',
+        }
+    }
+
+    const { id } = data
+
+    let board
+    try {
+        board = await db.board.delete({
+            where: {
+                id,
+                orgId
+            }
+        })
+    } catch (error) {
+        return {
+            error: 'Faile to delete'
+        }
+    }
+
+    revalidatePath(`/organization/${orgId}`)
+    redirect(`/organization/${orgId}`)
+}
+
+export const deleteBoard = createSafeAction(DeleteBoard, handler)
