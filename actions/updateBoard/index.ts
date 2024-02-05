cd1bbeee2be3b430ae4fcf230426dc6116ad8971
Othamae <vero.gconesa@gmail.com>
2024-02-05 12:16:49 +0100

    feat: :sparkles: Created UpdateActions

---
+++ actions/updateBoard/index.ts
@@ -0,0 +1,41 @@
+'use server'
+import { db } from '@/lib/db'
+
+import { auth } from '@clerk/nextjs'
+import { revalidatePath } from 'next/cache'
+import { createSafeAction } from '../createSafeAction'
+import { UpdateBoard } from './schema'
+import { InputType, ReturnType } from './types'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+    const { userId, orgId } = auth()
+    if (!userId || !orgId) {
+        return {
+            error: 'Unauthorized',
+        }
+    }
+
+    const { title, id } = data
+
+    let board
+    try {
+        board = await db.board.update({
+            where: {
+                id,
+                orgId
+            },
+            data: {
+                title
+            }
+        })
+    } catch (error) {
+        return {
+            error: 'Faile to update'
+        }
+    }
+
+    revalidatePath(`/board/${id}`)
+    return { data: board }
+}
+
+export const updateBoard = createSafeAction(UpdateBoard, handler)
---
+++ actions/updateBoard/types.ts
@@ -0,0 +1,8 @@
+import { Board } from "@prisma/client"
+import { z } from "zod"
+
+import { ActionState } from "../createSafeAction"
+import { UpdateBoard } from "./schema"
+
+export type InputType = z.infer<typeof UpdateBoard>
+export type ReturnType = ActionState<InputType, Board>
