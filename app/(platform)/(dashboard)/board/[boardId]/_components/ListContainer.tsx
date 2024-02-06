ebc2ab40d10879637ec9ee42bdd9288fc3096418
Othamae <vero.gconesa@gmail.com>
2024-02-06 11:12:04 +0100

    feat: :sparkles: Added ordererList in ListContainer

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { ListWithCards } from '@/lib/types'
+import { useEffect, useState } from 'react'
 import ListForm from './ListForm'
+import ListItem from './ListItem'
 
 interface ListContainerProps {
 	boardId: string
@@ -9,8 +11,18 @@ interface ListContainerProps {
 }
 
 const ListContainer = ({ boardId, list }: ListContainerProps) => {
+	const [orderedList, setOrderedList] = useState(list)
+	useEffect(() => {
+		setOrderedList(list)
+	}, [list])
 	return (
-		<ol>
+		<ol className='flex gap-x-3 h-full'>
+			{orderedList.map((list, index) => (
+				<ListItem key={list.id}
+					index={index}
+					list={list}
+				/>
+			))}
 			<ListForm />
 			<div className="flex-shrink-0 w-1" />
 		</ol>
