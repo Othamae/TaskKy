25092152e4cc8d6a6acba8466b2ed278250cf6de
Othamae <vero.gconesa@gmail.com>
2024-02-06 16:34:26 +0100

    feat: :sparkles: Added drag and drop in CardItem component

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
@@ -1,5 +1,7 @@
 'use client'
+import { Draggable } from '@hello-pangea/dnd'
 import { Card } from '@prisma/client'
+
 interface CardItemProps {
     index: number
     card: Card
@@ -7,11 +9,19 @@ interface CardItemProps {
 
 const CardItem = ({ index, card }: CardItemProps) => {
     return (
-        <div
-            role='button'
-            className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'>
-            {card.title}
-        </div>
+        <Draggable draggableId={card.id} index={index}>
+            {(provided) => (
+                <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    role='button'
+                    className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'>
+                    {card.title}
+                </div>
+            )}
+
+        </Draggable>
     )
 }
 
