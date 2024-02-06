0d2445840dd05315f7f47e786dce44a7499d998d
Othamae <vero.gconesa@gmail.com>
2024-02-06 14:56:04 +0100

    feat: :sparkles: Added CardItem in ListItem

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import { ListWithCards } from '@/lib/types'
+import { cn } from '@/lib/utils'
 import { ElementRef, useRef, useState } from 'react'
 import CardForm from './CardForm'
+import CardItem from './CardItem'
 import ListHeader from './ListHeader'
 
 interface ListItemProps {
@@ -28,13 +30,24 @@ const ListItem = ({ index, list }: ListItemProps) => {
         <li className='shrink-0 h-full w-[272px] select-none'>
             <div className='w-full rounded-md bg-[#f1f2f4] shadow-md pb-2'>
                 <ListHeader list={list} onAddCard={enableEditing} />
+                <ol className={cn(
+                    'mx-1 px-1 py-0.5 flex flex-col gap-y-2',
+                    list.cards.length > 0 ? 'mt-2' : 'mt-0'
+                )}>
+                    {
+                        list.cards.map((card, index) => (
+                            <CardItem key={card.id} index={index} card={card} />
+                        ))
+                    }
+                </ol>
                 <CardForm
                     ref={textAreaRef}
                     listId={list.id}
                     isEditing={isEditing}
                     enableEditing={enableEditing}
                     disableEditing={desableEditing}
-                ></CardForm>
+                />
+
             </div>
         </li>
     )
