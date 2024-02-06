d4255f348d96f842e1c7d950baeccfe776b44c16
Othamae <vero.gconesa@gmail.com>
2024-02-06 14:14:04 +0100

    feat: :sparkles: Added cardForm in ListItems

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { ListWithCards } from '@/lib/types'
+import { ElementRef, useRef, useState } from 'react'
+import CardForm from './CardForm'
 import ListHeader from './ListHeader'
 
 interface ListItemProps {
@@ -9,10 +11,30 @@ interface ListItemProps {
 }
 
 const ListItem = ({ index, list }: ListItemProps) => {
+    const textAreaRef = useRef<ElementRef<'textarea'>>(null)
+    const [isEditing, setIsEditing] = useState(false)
+
+    const desableEditing = () => {
+        setIsEditing(false)
+    }
+
+    const enableEditing = () => {
+        setIsEditing(true)
+        setTimeout(() => {
+            textAreaRef.current?.focus()
+        })
+    }
     return (
         <li className='shrink-0 h-full w-[272px] select-none'>
             <div className='w-full rounded-md bg-[#f1f2f4] shadow-md pb-2'>
-                <ListHeader list={list} />
+                <ListHeader list={list} onAddCard={enableEditing} />
+                <CardForm
+                    ref={textAreaRef}
+                    listId={list.id}
+                    isEditing={isEditing}
+                    enableEditing={enableEditing}
+                    disableEditing={desableEditing}
+                ></CardForm>
             </div>
         </li>
     )
