8d6bd0f3d1e00f1600e9884cd87ac23d3dc7bf74
Othamae <vero.gconesa@gmail.com>
2024-02-07 10:01:42 +0100

    refactor: :recycle: Refactored ListForm

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListForm.tsx
@@ -1,58 +1,14 @@
 'use client'
 
-import { createList } from '@/actions/createList'
 import FormInput from '@/components/form/FormInput'
 import FormSubmitButton from '@/components/form/FormSubmitButton'
 import { Button } from '@/components/ui/button'
-import { useAction } from '@/hooks/useAction'
+import { useListForm } from '@/hooks/useListForm'
 import { Plus, X } from 'lucide-react'
-import { useParams, useRouter } from 'next/navigation'
-import { ElementRef, useRef, useState } from 'react'
-import { toast } from 'sonner'
-import { useEventListener, useOnClickOutside } from 'usehooks-ts'
 import ListWrapper from './ListWrapper'
 
 const ListForm = () => {
-	const params = useParams()
-	const router = useRouter()
-	const [isEditing, setIsEditing] = useState(false)
-	const formRef = useRef<ElementRef<'form'>>(null)
-	const inputRef = useRef<ElementRef<'input'>>(null)
-
-	const enableEditing = () => {
-		setIsEditing(true)
-		setTimeout(() => {
-			inputRef.current?.focus()
-		})
-	}
-
-	const disableEditing = () => {
-		setIsEditing(false)
-	}
-
-	const { execute, fieldErrors } = useAction(createList, {
-		onSuccess: (data) => {
-			toast.success(`List "${data.title}" created`)
-			disableEditing()
-			router.refresh()
-		},
-		onError: (error) => {
-			toast.error(error)
-		},
-	})
-
-	const onKeyDown = (e: KeyboardEvent) => {
-		if (e.key === 'Escape') disableEditing()
-	}
-
-	useEventListener('keydown', onKeyDown)
-	useOnClickOutside(formRef, disableEditing)
-
-	const onSubmit = (formData: FormData) => {
-		const title = formData.get('title') as string
-		const boardId = formData.get('boardId') as string
-		execute({ title, boardId })
-	}
+	const { disableEditing, isEditing, inputRef, formRef, onSubmit, fieldErrors, params, enableEditing } = useListForm()
 
 	if (isEditing) {
 		return (
