56acc91758306deb39ff9a11187aecd98ccaa46e
Othamae <vero.gconesa@gmail.com>
2024-02-07 10:01:29 +0100

    feat: :recycle: Refactored ListHeader

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import { updateList } from '@/actions/updateList'
 import FormInput from '@/components/form/FormInput'
-import { useAction } from '@/hooks/useAction'
+import { useListHeader } from '@/hooks/useListHeader'
 import { List } from '@prisma/client'
-import { ElementRef, useRef, useState } from 'react'
-import { toast } from 'sonner'
-import { useEventListener } from 'usehooks-ts'
 import ListOptions from './ListOptions'
 
 interface ListHeaderProps {
@@ -14,54 +10,7 @@ interface ListHeaderProps {
     onAddCard: () => void
 }
 const ListHeader = ({ list, onAddCard }: ListHeaderProps) => {
-    const [title, setTitle] = useState(list.title)
-    const [isEditing, setIsEditing] = useState(false)
-
-    const formRef = useRef<ElementRef<'form'>>(null)
-    const inputRef = useRef<ElementRef<'input'>>(null)
-
-    const enableEditing = () => {
-        setIsEditing(true)
-        setTimeout(() => {
-            inputRef.current?.focus()
-            inputRef.current?.select()
-        })
-    }
-
-    const disableEditing = () => {
-        setIsEditing(false)
-    }
-
-    const { execute } = useAction(updateList, {
-        onSuccess: (data) => {
-            toast.success(`Renamed to "${data.title}"`)
-            setTitle(data.title)
-            disableEditing()
-        },
-        onError: (error) => {
-            toast.error(error)
-        }
-    })
-    const onKeyDown = (e: KeyboardEvent) => {
-        if (e.key === 'Escape') {
-            formRef.current?.submit()
-        }
-    }
-
-    const handleSubmit = (formData: FormData) => {
-        const title = formData.get('title') as string
-        const boardId = formData.get('boardId') as string
-        const id = formData.get('id') as string
-        if (title === list.title) return disableEditing()
-
-        execute({ title, boardId, id })
-    }
-    const onBlur = () => {
-        formRef.current?.requestSubmit()
-    }
-
-    useEventListener('keydown', onKeyDown)
-
+    const { title, onBlur, handleSubmit, enableEditing, inputRef, isEditing, formRef } = useListHeader(list.title)
     return (
         <div className='pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2'>
             {
