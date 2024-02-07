58e032c9df4a9e0953a54ae4f284f5a0b55f9b59
Othamae <vero.gconesa@gmail.com>
2024-02-07 10:01:56 +0100

    refactor: :recycle: Refactored ListContainer

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import { updateCardOrder } from '@/actions/updateCardOrder'
-import { updateListOrder } from '@/actions/updateListOrder'
-import { useAction } from '@/hooks/useAction'
+import { UseListContainer } from '@/hooks/useListContainer'
 import { ListWithCards } from '@/lib/types'
 import { DragDropContext, Droppable } from '@hello-pangea/dnd'
-import { useEffect, useState } from 'react'
-import { toast } from 'sonner'
 import ListForm from './ListForm'
 import ListItem from './ListItem'
 
@@ -15,95 +11,9 @@ interface ListContainerProps {
 	list: ListWithCards[]
 }
 
-function reorder<T>(list: T[], startIndex: number, endIndex: number) {
-	const result = Array.from(list)
-	const [removed] = result.splice(startIndex, 1)
-	result.splice(endIndex, 0, removed)
-
-	return result
-}
 
 const ListContainer = ({ boardId, list }: ListContainerProps) => {
-	const [orderedList, setOrderedList] = useState(list)
-
-	const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
-		onSuccess: () => {
-			toast.success('List reordered')
-		},
-		onError: (error) => {
-			toast.error(error)
-		}
-	})
-
-	const { execute: executeUpdateCardOrder } = useAction(updateCardOrder, {
-		onSuccess: () => {
-			toast.success('Card reordered')
-		},
-		onError: (error) => {
-			toast.error(error)
-		}
-	})
-	useEffect(() => {
-		setOrderedList(list)
-	}, [list])
-
-	const onDragEnd = (result: any) => {
-		const { destination, source, type } = result
-		if (!destination) return
-		// if Dropped in the same position
-		if (destination.droppableId === source.droppableId && destination.index === source.index) return
-
-		if (type === 'list') {
-			const newOrderedList = reorder(orderedList, source.index, destination.index).map((item, index) => ({ ...item, order: index }))
-			setOrderedList(newOrderedList)
-
-			executeUpdateListOrder({ items: newOrderedList, boardId })
-		}
-
-		if (type === 'card') {
-			let newOrderedList = [...orderedList]
-			const sourceList = newOrderedList.find(list => list.id === source.droppableId)
-			const destinationList = newOrderedList.find(list => list.id === destination.droppableId)
-
-			if (!sourceList || !destinationList) return
-
-			if (!sourceList.cards) sourceList.cards = []
-
-			if (!destinationList.cards) destinationList.cards = []
-
-			// Moving card in same list
-			if (source.droppableId === destination.droppableId) {
-				const reorderedCards = reorder(sourceList.cards, source.index, destination.index)
-				reorderedCards.forEach((card, index) => card.order = index)
-				sourceList.cards = reorderedCards
-				setOrderedList(newOrderedList)
-
-				executeUpdateCardOrder({ items: reorderedCards, boardId })
-
-			} else { // Moving card between lists
-				const [movedCard] = sourceList.cards.splice(source.index, 1)
-
-				// Assign destination listId to the moved card
-				movedCard.listId = destination.droppableId
-
-				// Add moved card to destination list
-				destinationList.cards.splice(destination.index, 0, movedCard)
-
-				sourceList.cards.forEach((card, index) => card.order = index)
-
-				destinationList.cards.forEach((card, index) => card.order = index)
-
-				setOrderedList(newOrderedList)
-
-				executeUpdateCardOrder({ items: destinationList.cards, boardId })
-
-			}
-
-
-
-		}
-
-	}
+	const { onDragEnd, orderedList } = UseListContainer({ boardId, list })
 	return (
 		<DragDropContext onDragEnd={onDragEnd}>
 			<Droppable droppableId='lists' type='list' direction='horizontal'>
