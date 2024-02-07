9d034c6a12bded5f58cd00f767aa5fbaa42229f4
Othamae <vero.gconesa@gmail.com>
2024-02-07 10:03:08 +0100

    feat: :sparkles: Added cardModal in CardItem

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useCardModal } from '@/hooks/useCardModal'
 import { Draggable } from '@hello-pangea/dnd'
 import { Card } from '@prisma/client'
 
@@ -8,6 +9,7 @@ interface CardItemProps {
 }
 
 const CardItem = ({ index, card }: CardItemProps) => {
+    const cardModal = useCardModal()
     return (
         <Draggable draggableId={card.id} index={index}>
             {(provided) => (
@@ -16,6 +18,7 @@ const CardItem = ({ index, card }: CardItemProps) => {
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
                     role='button'
+                    onClick={() => cardModal.onOpen(card.id)}
                     className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'>
                     {card.title}
                 </div>
