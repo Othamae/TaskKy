67bb60b393b3b2d9eb4313046cdad390dd103594
Othamae <vero.gconesa@gmail.com>
2024-02-07 14:43:43 +0100

    feat: :sparkles: Changed title to optional in updateCard schema

--- actions/updateCard/schema.ts
+++ actions/updateCard/schema.ts
@@ -1,14 +1,15 @@
 import { z } from 'zod'
 
 export const UpdateCard = z.object({
-	title: z
-		.string({
+	title: z.optional(
+		z.string({
 			required_error: 'Title is required',
 			invalid_type_error: 'Title is required',
 		})
-		.min(3, {
-			message: 'Title must be at least 3 characters',
-		}),
+			.min(3, {
+				message: 'Title must be at least 3 characters',
+			}),
+	),
 	id: z.string(),
 	boardId: z.string(),
 	description: z.optional(
