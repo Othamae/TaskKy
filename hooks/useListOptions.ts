9270f409921b42ada13ab2d324f2533670250fbf
Othamae <vero.gconesa@gmail.com>
2024-02-07 09:54:17 +0100

    feat: :sparkles: Created useListOptions hook

---
+++ hooks/useListOptions.ts
@@ -0,0 +1,46 @@
+'use client'
+import { copyList } from '@/actions/copyList'
+import { deleteList } from '@/actions/deleteList'
+import { useAction } from '@/hooks/useAction'
+import { ElementRef, useRef } from 'react'
+import { toast } from 'sonner'
+
+export const UseListOptions = () => {
+    const closeRef = useRef<ElementRef<'button'>>(null)
+    const { execute: executeDelete } = useAction(deleteList, {
+        onSuccess: (data) => {
+            toast.success(`List "${data.title}" deleted successfully`)
+            closeRef.current?.click()
+        },
+        onError: (error) => {
+            toast.error(error)
+        }
+    })
+    const { execute: executeCopy } = useAction(copyList, {
+        onSuccess: (data) => {
+            toast.success(`List "${data.title}" copied`)
+            closeRef.current?.click()
+        },
+        onError: (error) => {
+            toast.error(error)
+        }
+    })
+
+    const handleDelete = (formData: FormData) => {
+        const id = formData.get('id') as string
+        const boardId = formData.get('boardId') as string
+        executeDelete({ id, boardId })
+    }
+
+    const handleCopy = (formData: FormData) => {
+        const id = formData.get('id') as string
+        const boardId = formData.get('boardId') as string
+        executeCopy({ id, boardId })
+    }
+
+    return {
+        handleDelete,
+        handleCopy,
+        closeRef
+    }
+}
