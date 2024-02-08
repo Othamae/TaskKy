0c843e70b7c3a27911e364083599c9b89311ca90
Othamae <vero.gconesa@gmail.com>
2024-02-08 20:55:30 +0100

    feat: :sparkles: Created stripe const

---
+++ const/stripe.ts
@@ -0,0 +1,9 @@
+export const CURRENCY = 'CZK'
+export const PRODUCT_DATA_NAME = 'TaskKy Pro'
+export const PRODUCT_DATA_DESCRIPTION = 'Unlimited boards for your organization'
+export const UNIT_AMOUNT = 20000
+export const RECURRING_INTERVAL = 'month'
+export const QUANTITY = 1
+export const MODE = 'subscription'
+export const PAYMENT_TYPES = 'card'
+export const BILLING_ADDRESS_COLLECTION = 'auto'
