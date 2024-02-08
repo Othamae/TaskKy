d26f6a7f1f670f29272b11d2d7df1517e8e4a61c
Othamae <vero.gconesa@gmail.com>
2024-02-08 20:28:15 +0100

    refactor: :recycle: Added useFormPicker in FromPicker component

--- components/form/FormPicker.tsx
+++ components/form/FormPicker.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import { defaultImages } from '@/const/images'
-import { unsplash } from '@/lib/unsplash'
+import { useFormPicker } from '@/hooks/useFormPicker'
 import { cn } from '@/lib/utils'
 import { Check, Loader2 } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { useFormStatus } from 'react-dom'
 import FormErrors from './FormErrors'
 
 interface FormPickerProps {
@@ -16,83 +13,54 @@ interface FormPickerProps {
 }
 
 const FormPicker = ({ id, errors }: FormPickerProps) => {
-	const { pending } = useFormStatus()
-	const [images, setImages] = useState<Array<Record<string, any>>>(defaultImages)
-	const [isLoading, setIsLoading] = useState(true)
-	const [selectedImageId, setSelectedImageId] = useState(null)
+	const { images, isLoading, pending, selectedImageId, handleClick } = useFormPicker()
 
-	useEffect(() => {
-		const fetchImages = async () => {
-			try {
-				const result = await unsplash.photos.getRandom({
-					collectionIds: ['317099'],
-					count: 9,
-				})
-				if (result && result.response) {
-					const imagesCollection = result.response as Array<Record<string, any>>
-					setImages(imagesCollection)
-				} else {
-					console.log('Failed to get images from Unsplash')
-				}
-			} catch (error) {
-				console.log({ error })
-			} finally {
-				setIsLoading(false)
-			}
-		}
-		fetchImages()
-	}, [])
-
-	if (isLoading) {
-		return (
+	return (
+		isLoading
+			?
 			<div className='p-6 flex items-center justify-center'>
 				<Loader2 className='h-6 w-6 text-sky-700 animate-spin' />
 			</div>
-		)
-	}
-
-	return (
-		<div className='relative'>
-			<div className='grid grid-cols-3 gap-2 mb-2'>
-				{images.map((image) => (
-					<div
-						key={image.id}
-						className={cn(
-							'cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted',
-							pending && 'opacity-50 hover:opacity-50 cursor-auto',
-						)}
-						onClick={() => {
-							if (pending) return
-							setSelectedImageId(image.id)
-						}}
-					>
-						<input
-							type='radio'
-							id={id}
-							name={id}
-							className='hidden'
-							checked={selectedImageId === image.id}
-							disabled={pending}
-							value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
-						/>
-						<Image fill alt='Unsplash image' className='object-cover rounded-sm' src={image.urls.thumb} />
-						{selectedImageId === image.id && (
-							<div className='absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center'>
-								<Check className='h-4 w-4 text-white' />
-							</div>
-						)}
-						<Link
-							href={image.links.html}
-							target='_blank'
-							className='opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50'
+			:
+			<div className='relative'>
+				<div className='grid grid-cols-3 gap-2 mb-2'>
+					{images.map((image) => (
+						<div
+							key={image.id}
+							className={cn(
+								'cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted',
+								pending && 'opacity-50 hover:opacity-50 cursor-auto',
+							)}
+							onClick={() => handleClick(image.id)}
 						>
-							{image.user.name}
-						</Link>
-					</div>
-				))}
+							<input
+								type='radio'
+								id={id}
+								name={id}
+								className='hidden'
+								checked={selectedImageId === image.id}
+								disabled={pending}
+								value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
+							/>
+							<Image fill alt='Unsplash image' className='object-cover rounded-sm' src={image.urls.thumb} />
+							{selectedImageId === image.id && (
+								<div className='absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center'>
+									<Check className='h-4 w-4 text-white' />
+								</div>
+							)}
+							<Link
+								href={image.links.html}
+								target='_blank'
+								className='opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50'
+							>
+								{image.user.name}
+							</Link>
+						</div>
+					))}
+				</div>
+				<FormErrors id='image' errors={errors} />
 			</div>
-			<FormErrors id='image' errors={errors} />
-		</div>
+
 	)
 }
 
