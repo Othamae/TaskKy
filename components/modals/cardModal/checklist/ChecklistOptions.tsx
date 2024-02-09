08aaaa7b3a86f66ac762bcf16cb988d9e85fa845
Othamae <vero.gconesa@gmail.com>
2024-02-09 18:51:22 +0100

    feat: :sparkles: Created ChecklistOptions

---
+++ components/modals/cardModal/checklist/ChecklistOptions.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import FormSubmitButton from '@/components/form/FormSubmitButton'
+import { Button } from '@/components/ui/button'
+import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Separator } from '@/components/ui/separator'
+import { ADD_TASK, CHECKLIST_ACTIONS, COPY_CHECKLIST, DELETE_THIS_CHECKLIST } from '@/const/const'
+import { useOptions } from '@/hooks/useOptions'
+import { ChecklistWithTasks } from '@/lib/types'
+import { MoreHorizontal, X } from 'lucide-react'
+
+interface ChecklistOptionsProps {
+    checklist: ChecklistWithTasks
+    onAddTask: () => void
+}
+const ChecklistOptions = ({ checklist, onAddTask }: ChecklistOptionsProps) => {
+    const { handleCopy, handleDelete, closeRef } = useOptions({ type: 'Checklist' })
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button className='h-auto w-auto p-2' variant='ghost'>
+                    <MoreHorizontal className='h-4 w-4' />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className='px-0 pt-3 pb-3' side='bottom' align='start'>
+                <div className='text-sm font-medium text-center text-neutral-600 pb-4'>{CHECKLIST_ACTIONS}</div>
+                <PopoverClose asChild ref={closeRef}>
+                    <Button className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600' variant='ghost'>
+                        <X className='h-4 w-4' />
+                    </Button>
+                </PopoverClose>
+                <Button
+                    onClick={onAddTask}
+                    className='rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm'
+                    variant='ghost'
+                >
+                    {ADD_TASK}...
+                </Button>
+                <form action={handleCopy}>
+                    <input hidden id='id' name='id' value={checklist.id} />
+                    <input hidden id='cardId' name='cardId' value={checklist.cardId} />
+                    <FormSubmitButton
+                        className='rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm'
+                        variant='ghost'
+                    >
+                        {COPY_CHECKLIST}...
+                    </FormSubmitButton>
+                </form>
+                <Separator />
+                <form action={handleDelete}>
+                    <input hidden id='id' name='id' value={checklist.id} />
+                    <input hidden id='cardId' name='cardId' value={checklist.cardId} />
+                    <FormSubmitButton
+                        className='rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm'
+                        variant='ghost'
+                    >
+                        {DELETE_THIS_CHECKLIST}...
+                    </FormSubmitButton>
+                </form>
+            </PopoverContent>
+        </Popover>
+    )
+}
+
+export default ChecklistOptions
