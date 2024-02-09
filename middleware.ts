8e5a9fe74852b1742698953afc40ebc9b35df5b1
Othamae <vero.gconesa@gmail.com>
2024-02-09 19:01:00 +0100

    feat: :sparkles: Updated middleware to add as publicRoutes api/webhook

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,17 @@
 import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
+import { API_WEBHOOK, HOME, ORGANIZATION, SELECT_ORG } from './const/routes'
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-	publicRoutes: ['/'],
+	publicRoutes: [HOME, API_WEBHOOK],
 	afterAuth(auth, req) {
 		if (auth.userId && auth.isPublicRoute) {
-			let path = '/select-org'
+			let path = SELECT_ORG
 			if (auth.orgId) {
-				path = `/organization/${auth.orgId}`
+				path = `${ORGANIZATION}/${auth.orgId}`
 			}
 			const orgSelection = new URL(path, req.url)
 			return NextResponse.redirect(orgSelection)
@@ -18,8 +19,8 @@ export default authMiddleware({
 		if (!auth.userId && !auth.isPublicRoute) {
 			return redirectToSignIn({ returnBackUrl: req.url })
 		}
-		if (auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org') {
-			const orgSelection = new URL('/select-org', req.url)
+		if (auth.userId && !auth.orgId && req.nextUrl.pathname !== SELECT_ORG) {
+			const orgSelection = new URL(SELECT_ORG, req.url)
 			return NextResponse.redirect(orgSelection)
 		}
 	},
