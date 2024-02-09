b6cf68d3042954c5fab19990dd0874fff337fd14
Othamae <vero.gconesa@gmail.com>
2024-02-09 19:17:56 +0100

    feat: :recycle: Refactoe CardForm

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
@@ -3,7 +3,7 @@
 import FormSubmitButton from '@/components/form/FormSubmitButton'
 import FormTextarea from '@/components/form/FormTextarea'
 import { Button } from '@/components/ui/button'
-import { ADD_A_CARD, ADD_CARD, ENTER_A_TITTLE_PLACEHOLDER } from '@/const/const'
+import { ADD_A_CARD, ADD_CARD, ENTER_A_CARD_TITTLE_PLACEHOLDER } from '@/const/const'
 import { useCardForm } from '@/hooks/useCardForm'
 import { Plus, X } from 'lucide-react'
 import { forwardRef } from 'react'
@@ -19,14 +19,15 @@ const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
 	({ isEditing, enableEditing, disableEditing, listId }, ref) => {
 		const { onSubmit, onTextareaKeyDown, formRef, fieldErrors } = useCardForm(disableEditing)
 
-		if (isEditing) {
-			return (
+		return (
+			isEditing
+				?
 				<form ref={formRef} action={onSubmit} className='m-1 py-0.5 px-1 space-y-4'>
 					<FormTextarea
 						id='title'
 						onKeyDown={onTextareaKeyDown}
 						ref={ref}
-						placeholder={ENTER_A_TITTLE_PLACEHOLDER}
+						placeholder={ENTER_A_CARD_TITTLE_PLACEHOLDER}
 						errors={fieldErrors}
 					/>
 					<input name='listId' hidden id='listId' value={listId} />
@@ -37,21 +38,18 @@ const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
 						</Button>
 					</div>
 				</form>
-			)
-		}
-
-		return (
-			<div className='pt-2 px-2'>
-				<Button
-					onClick={enableEditing}
-					className='h-auto px-2 py-2.5 w-full justify-start text-muted-foreground text-sm'
-					size='sm'
-					variant='ghost'
-				>
-					<Plus className='w-4 h-4 mr-2'></Plus>
-					{ADD_A_CARD}
-				</Button>
-			</div>
+				:
+				<div className='pt-2 px-2'>
+					<Button
+						onClick={enableEditing}
+						className='h-auto px-2 py-2.5 w-full justify-start text-muted-foreground text-sm'
+						size='sm'
+						variant='ghost'
+					>
+						<Plus className='w-4 h-4 mr-2'></Plus>
+						{ADD_A_CARD}
+					</Button>
+				</div>
 		)
 	},
 )
