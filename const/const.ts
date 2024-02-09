cea053a26833e0514d0e93128a88cc255fc615c0
Othamae <vero.gconesa@gmail.com>
2024-02-09 19:06:10 +0100

    feat: :sparkles: Created const file

---
+++ const/const.ts
@@ -0,0 +1,52 @@
+export const MAX_FREE_BOARDS = 5
+
+export const CREATE = 'Create'
+export const COPY = 'Copy'
+export const DELETE = 'Delete'
+
+export const TYPE_BOARD = 'Board'
+export const TYPE_LIST = 'List'
+export const TYPE_CARD = 'Card'
+export const TYPE_CHECKLIST = 'Checklist'
+export const TYPE_TASK = 'Task'
+
+export const MANAGE_SUBSCRIPTION = 'Manage Subscription'
+export const UPGRADE_PRO = 'Upgrade to Pro'
+
+export const SUCCESS_CREATED = 'Created'
+export const SUCCESS_COPY = 'Copied'
+export const SUCCESS_DELETED = 'Deleted'
+export const SUCCESS_UPDATED = 'Updated'
+
+export const WORKSPACES = 'Workspaces'
+
+// BOARD
+export const BOARD_OPTIONS = 'Board options'
+export const DELETE_BOARD = 'Delete board'
+export const CREATE_BOARD = 'Create board'
+export const YOUR_BOARDS = 'Your boards'
+export const CREATE_NEW_BOARD = 'Create new board'
+
+// LIST
+export const ADD_LIST = 'Add list'
+export const ADD_A_LIST = 'Add a list'
+export const ENTER_A_LIST_TITTLE_PLACEHOLDER = 'Enter list title...'
+export const DELETE_THIS_LIST = 'Delete this list'
+export const COPY_LIST = 'Copy list'
+
+// CARD
+export const ADD_CARD = 'Add card'
+export const ADD_A_CARD = 'Add a card'
+export const ENTER_A_CARD_TITTLE_PLACEHOLDER = 'Enter a title for this card...'
+
+// CHECKLIST
+export const COPY_CHECKLIST = 'Copy checklist'
+export const ENTER_A_CHECKLIST_TITTLE_PLACEHOLDER = 'Enter a title for this checklist...'
+export const ADD_CHECKLIST = 'Add checklist'
+export const DELETE_THIS_CHECKLIST = 'Delete this checklist'
+export const CHECKLIST_ACTIONS = 'Checklist Actions'
+
+// TASK
+export const ADD_TASK = 'Add task'
+export const ADD_A_TASK = 'Add a task'
+
