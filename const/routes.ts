fc13f81e9e8d35d475ef98312f4125f3deb891d8
Othamae <vero.gconesa@gmail.com>
2024-02-09 19:05:56 +0100

    feat: :sparkles: Created routes const file

---
+++ const/routes.ts
@@ -0,0 +1,6 @@
+export const SELECT_ORG = '/select-org'
+export const BOARD = '/board'
+export const ORGANIZATION = '/organization'
+export const HOME = '/'
+
+export const API_WEBHOOK = '/api/webhook'
