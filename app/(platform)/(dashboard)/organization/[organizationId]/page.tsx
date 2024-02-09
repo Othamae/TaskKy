90024d304114439d92c709f39f8a7e43dae6e52b
Othamae <vero.gconesa@gmail.com>
2024-02-09 20:02:23 +0100

    feat: :sparkles: Added isPro to OrgInfo in OrganizationIdPage

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,12 +1,14 @@
 import { Separator } from '@/components/ui/separator'
+import { checkSubscription } from '@/lib/helpers/subscription'
 import { Suspense } from 'react'
 import BoardList from './_components/BoardList'
 import OrgInfo from './_components/OrgInfo'
 
 const OrganizationIdPage = async () => {
+	const isPro = await checkSubscription()
 	return (
 		<div className='w-full mb-20'>
-			<OrgInfo />
+			<OrgInfo isPro={isPro} />
 			<Separator className='my-4' />
 			<div className='px-2 md:px-4'>
 				<Suspense fallback={<BoardList.skeleton />}>
