59b9aa8902ee53c357c5ef312caa0cfc2836ff6f
Othamae <vero.gconesa@gmail.com>
2024-02-09 20:17:51 +0100

    feat: :recycle: Refactored createAuditLog helper

--- lib/helpers/createAuditLog.ts
+++ lib/helpers/createAuditLog.ts
@@ -1,6 +1,7 @@
 import { auth, currentUser } from '@clerk/nextjs'
 import { ACTION, ENTITY_TYPE } from '@prisma/client'
 
+import { ERROR_USER_NOT_FOUND } from '@/const/errorMessages'
 import { db } from '../db'
 
 interface Props {
@@ -14,8 +15,7 @@ export const createAuditLog = async ({ entityId, entityTitle, entityType, action
     try {
         const { orgId } = auth()
         const user = await currentUser()
-        if (!user || !orgId) throw new Error('User not found!')
-
+        if (!user || !orgId) throw new Error(ERROR_USER_NOT_FOUND)
         await db.auditLog.create({
             data: {
                 entityId,
