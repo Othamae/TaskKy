a068a82420f826a25f32bc2085ad38a4b9e4da30
Othamae <vero.gconesa@gmail.com>
2024-02-09 20:01:47 +0100

    feat: :sparkles: Added isPro to OrgInfo in ActivityPage

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -1,12 +1,14 @@
 import { Separator } from '@/components/ui/separator'
+import { checkSubscription } from '@/lib/helpers/subscription'
 import { Suspense } from 'react'
 import OrgInfo from '../_components/OrgInfo'
 import ActivityList from './_components/ActivityList'
 
-const ActivityPage = () => {
+const ActivityPage = async () => {
+	const isPro = await checkSubscription()
 	return (
 		<div className='w-full'>
-			<OrgInfo />
+			<OrgInfo isPro={isPro} />
 			<Separator className='my-2' />
 			<Suspense fallback={<ActivityList.Skeleton />}>
 				<ActivityList />
