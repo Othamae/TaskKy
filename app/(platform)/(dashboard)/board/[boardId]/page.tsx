cd2f32cb5d076967ae179a8a78d6cd30c78ba2a6
Othamae <vero.gconesa@gmail.com>
2024-02-09 19:38:31 +0100

    feat: :sparkles: Refactored BoardIdPage

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,6 +1,4 @@
-import { db } from '@/lib/db'
-import { auth } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
+import { getListsForBoardIdPage } from '@/lib/helpers/dbHelpers'
 import ListContainer from './_components/ListContainer'
 
 interface BoardIdPageProps {
@@ -10,27 +8,9 @@ interface BoardIdPageProps {
 }
 
 const BoardIdPage = async ({ params }: BoardIdPageProps) => {
-	const { orgId } = auth()
-	if (!orgId) redirect('/select-org')
 	const boardId = params.boardId
-	const lists = await db.list.findMany({
-		where: {
-			boardId: params.boardId,
-			board: {
-				orgId,
-			},
-		},
-		include: {
-			cards: {
-				orderBy: {
-					order: 'asc',
-				},
-			},
-		},
-		orderBy: {
-			order: 'asc',
-		},
-	})
+	const lists = await getListsForBoardIdPage(boardId)
+
 	return (
 		<div className='p-4 h-full overflow-x-auto'>
 			<ListContainer boardId={boardId} list={lists} />
