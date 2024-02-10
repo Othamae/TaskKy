688faca916f37fd3285179b68fbbd5840d8809a1
Othamae <vero.gconesa@gmail.com>
2024-02-10 16:59:10 +0100

    feat: :sparkles: Created ChecklistHeader

---
+++ components/modals/cardModal/checklist/ChecklistHeader.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import FormInput from '@/components/form/FormInput'
+import { ENTER_A_CHECKLIST_TITTLE_PLACEHOLDER } from '@/const/const'
+import { useHeader } from '@/hooks/useHeader'
+import { ChecklistWithTasks } from '@/lib/types'
+import { CheckSquare } from 'lucide-react'
+import ChecklistOptions from './ChecklistOptions'
+
+interface ChecklistHeaderProps {
+    checklist: ChecklistWithTasks
+}
+
+const ChecklistHeader = ({ checklist }: ChecklistHeaderProps) => {
+    const { title, onBlur, handleSubmit, enableEditing, inputRef, isEditing, formRef } = useHeader({ defaultTitle: checklist.title, type: 'Checklist' })
+
+    return (
+        <div className='flex justify-between items-start gap-x-2 w-full'>
+            <CheckSquare className='h-5 w-5 mt-0.5 text-neutral-700' />
+            {isEditing ? (
+                <form action={handleSubmit} ref={formRef} className='flex-1 px-[2px]'>
+                    <input hidden id='id' name='id' value={checklist.id} />
+                    <input hidden id='cardId' name='cardId' value={checklist.cardId} />
+                    <FormInput
+                        ref={inputRef}
+                        id='title'
+                        onBlur={onBlur}
+                        placeholder={ENTER_A_CHECKLIST_TITTLE_PLACEHOLDER}
+                        defaultValue={title}
+                        className='text-base px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white'
+                    />
+                    <button hidden type='submit' />
+                </form>
+            ) : (
+                <div onClick={enableEditing}
+                    className='w-full text-base px-2.5 h-7 font-semibold border-transparent'>
+                    {title}
+                </div>
+            )}
+            <ChecklistOptions checklist={checklist} />
+        </div>
+    )
+}
+
+export default ChecklistHeader
