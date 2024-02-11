47666d49d485116ab36cde19918463ecbf947ede
Othamae <vero.gconesa@gmail.com>
2024-02-11 19:10:41 +0100

    refactor: :recycle: Added useHeader in header(card) component

--- components/modals/cardModal/header.tsx
+++ components/modals/cardModal/header.tsx
@@ -1,66 +1,31 @@
 'use client'
 
-import { updateCard } from '@/actions/updateCard'
 import FormInput from '@/components/form/FormInput'
 import { Skeleton } from '@/components/ui/skeleton'
-import { useAction } from '@/hooks/useAction'
+import { useHeader } from '@/hooks/useHeader'
 import { CardWithList } from '@/lib/types'
-import { useQueryClient } from '@tanstack/react-query'
 import { Layout } from 'lucide-react'
-import { useParams } from 'next/navigation'
-import { ElementRef, useRef, useState } from 'react'
-import { toast } from 'sonner'
 
 interface HeaderProps {
 	data: CardWithList
 }
 
 const Header = ({ data }: HeaderProps) => {
-	const queryClient = useQueryClient()
-	const params = useParams()
-	const inputRef = useRef<ElementRef<'input'>>(null)
-	const [title, setTitle] = useState(data.title)
-
-	const { execute } = useAction(updateCard, {
-		onSuccess: (data) => {
-			queryClient.invalidateQueries({
-				queryKey: ['card', data.id],
-			})
-			queryClient.invalidateQueries({
-				queryKey: ['card-logs', data.id],
-			})
-			toast.success(`Card "${data.title}" renamed`)
-			setTitle(data.title)
-		},
-		onError: (error) => {
-			toast.error(error)
-		},
-	})
-
-	const onBlur = () => {
-		inputRef.current?.form?.requestSubmit()
-	}
-
-	const onSubmit = (formData: FormData) => {
-		const title = formData.get('title') as string
-		const boardId = params.boardId as string
-
-		if (title === data.title) return
-
-		execute({ title, boardId, id: data.id })
-	}
+	const { handleSubmit, inputRef, title, onBlur, fieldErrorsCard } = useHeader({ type: 'Card', defaultTitle: data.title })
 	return (
 		<div className='flex items-start gap-x-3 mb-6 w-full'>
 			<Layout className='h-5 w-5 mt-1 text-neutral-700' />
 			<div className='w-full'>
-				<form action={onSubmit}>
+				<form action={handleSubmit}>
 					<FormInput
 						id='title'
 						ref={inputRef}
 						defaultValue={title}
 						onBlur={onBlur}
+						errors={fieldErrorsCard}
 						className='font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate'
 					/>
+					<input hidden id='id' name='id' value={data.id} />
 				</form>
 				<p className='text-sm text-muted-foreground'>
 					in list <span className='underline'>{data.list.title}</span>
