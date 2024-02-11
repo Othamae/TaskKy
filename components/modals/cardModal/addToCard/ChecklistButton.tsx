98bf779a1f763cf91923d37b68c02c9e460045f4
Othamae <vero.gconesa@gmail.com>
2024-02-11 17:16:06 +0100

    feat: :recycle: Added checklistForm to checklistButtonAdd

--- components/modals/cardModal/addToCard/ChecklistButton.tsx
+++ components/modals/cardModal/addToCard/ChecklistButton.tsx
@@ -1,19 +1,15 @@
 'use client'
-import FormInput from '@/components/form/FormInput'
-import FormSubmitButton from '@/components/form/FormSubmitButton'
 import { Button } from '@/components/ui/button'
 import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
-import { ADD_CHECKLIST, CREATE_CHECKLIST, ENTER_A_CHECKLIST_TITTLE_PLACEHOLDER, TYPE_CHECKLIST } from '@/const/const'
-import { useChecklistForm } from '@/hooks/useChecklistForm'
+import { CREATE_CHECKLIST, TYPE_CHECKLIST } from '@/const/const'
 import { CheckSquare, X } from 'lucide-react'
+import ChecklistForm from '../checklist/ChecklistForm'
 
 interface ChecklistButtonProps {
     cardId: string
 }
 
 const ChecklistButton = ({ cardId }: ChecklistButtonProps) => {
-    const { onSubmit, formRef, fieldErrors, closeRef } = useChecklistForm()
-
     return (
         <Popover>
             <PopoverTrigger asChild>
@@ -33,24 +29,7 @@ const ChecklistButton = ({ cardId }: ChecklistButtonProps) => {
                         <X className='h-4 w-4' />
                     </Button>
                 </PopoverClose>
-
-                <form ref={formRef} action={onSubmit} className='m-1 py-0.5 px-1 space-y-4'>
-                    <FormInput
-                        id='title'
-                        placeholder={ENTER_A_CHECKLIST_TITTLE_PLACEHOLDER}
-                        errors={fieldErrors}
-                        defaultValue={TYPE_CHECKLIST}
-                    />
-                    <input name='cardId' hidden id='cardId' value={cardId} />
-                    <div className='flex items-center gap-x-1'>
-                        <FormSubmitButton>{ADD_CHECKLIST}</FormSubmitButton>
-                        <PopoverClose asChild ref={closeRef}>
-                            <Button size='sm' variant='ghost'>
-                                <X className='h-5 w-5' />
-                            </Button>
-                        </PopoverClose>
-                    </div>
-                </form>
+                <ChecklistForm cardId={cardId} />               
             </PopoverContent>
         </Popover>
 
