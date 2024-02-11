d21b578e3b31bdf66d89eaf93ed0fae903cdfeac
Othamae <vero.gconesa@gmail.com>
2024-02-11 19:12:51 +0100

    feat: :sparkles: Added useCardModal in index cardModal

--- components/modals/cardModal/index.tsx
+++ components/modals/cardModal/index.tsx
@@ -2,38 +2,33 @@
 
 import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { useCardModal } from '@/hooks/useCardModal'
-import { fetcher } from '@/lib/fetcher'
-import { CardWithList } from '@/lib/types'
-import { AuditLog } from '@prisma/client'
-import { useQuery } from '@tanstack/react-query'
 import Actions from './Actions'
 import Activity from './Activity'
+import AddToCard from './AddToCard'
 import Description from './Description'
 import Header from './Header'
+import Checklist from './checklist/Checklist'
 
-const CardModal = () => {
-	const id = useCardModal((state) => state.id)
-	const isOpen = useCardModal((state) => state.isOpen)
-	const onClose = useCardModal((state) => state.onClose)
-	const { data: cardData } = useQuery<CardWithList>({
-		queryKey: ['card', id],
-		queryFn: () => fetcher(`/api/cards/${id}`),
-	})
-
-	const { data: auditLogsData } = useQuery<AuditLog[]>({
-		queryKey: ['card-logs', id],
-		queryFn: () => fetcher(`/api/cards/${id}/logs`),
-	})
+const CardModal = () => {	
+	const { isOpen, onClose, cardData, checkListData, auditLogsData } = useCardModal()
 	return (
 		<Dialog open={isOpen} onOpenChange={onClose}>
-			<DialogContent>
+			<DialogContent className='overflow-y-scroll max-h-screen no-scrollbar'>
 				{!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
 				<div className='grid grid-cols-1 md:grid-cols-4 md:gap-4'>
 					<div className='col-span-3'>
 						<div className='w-full space-y-6'>
+							<div className='grid grid-cols-1 md:grid-cols-4 md:gap-4'>
+								<p>Aqui labels</p>
+								<p>Aqui Dates</p>
+							</div>
 							{!cardData
 								? <Description.Skeleton />
 								: <Description data={cardData} />
+							}
+							{!checkListData || !cardData
+								? <Checklist.Skeleton />
+								: <Checklist items={checkListData} />
 							}
 							{!auditLogsData
 								? <Activity.Skeleton />
@@ -41,7 +36,10 @@ const CardModal = () => {
 							}
 						</div>
 					</div>
-					<div>{!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}</div>
+					<div>
+						{!checkListData || !cardData ? <AddToCard.Skeleton /> : <AddToCard checklistData={checkListData} cardData={cardData} />}
+						{!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
+					</div>
 				</div>
 			</DialogContent>
 		</Dialog>
