583f6e97fd49343b7790ca4ec6e133971fd43c3c
Othamae <vero.gconesa@gmail.com>
2024-02-11 17:23:31 +0100

    refactor: :recycle: Refactored using helpers

--- hooks/useChecklistForm.ts
+++ hooks/useChecklistForm.ts
@@ -1,6 +1,7 @@
 import { createChecklist } from '@/actions/createChecklist'
 import { SUCCESS_CREATED, TYPE_CHECKLIST } from '@/const/const'
 import { useAction } from '@/hooks/useAction'
+import { revalidateQueries } from '@/lib/helpers/helpers'
 import { useQueryClient } from '@tanstack/react-query'
 import { useParams } from 'next/navigation'
 import { ElementRef, useRef } from 'react'
@@ -18,15 +19,7 @@ export const useChecklistForm = () => {
 			toast.success(`${TYPE_CHECKLIST} "${data.title}" ${SUCCESS_CREATED}`)
 			formRef.current?.reset()
 			closeRef.current?.click()
-			queryClient.invalidateQueries({
-				queryKey: ['card', data.cardId],
-			})
-			queryClient.invalidateQueries({
-				queryKey: ['card-logs', data.cardId],
-			})
-			queryClient.invalidateQueries({
-				queryKey: ['card-checkList', data.cardId],
-			})
+			revalidateQueries(data.cardId, queryClient)
 		},
 		onError: (error) => {
 			toast.error(error)
