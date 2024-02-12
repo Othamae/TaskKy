777692c3d0fbe93c3a36b6b9011b72c97bf11be0
Othamae <vero.gconesa@gmail.com>
2024-02-12 14:42:53 +0100

    feat: ✨ Created DatePicker component

---
+++ components/DatePicker.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { useDueDate } from '@/hooks/useDueDate'
+import DatesForm from './modals/cardModal/dates/DatesForm'
+
+export function DatePicker() {
+    const { formRef, handleSubmit, date, onChange, setDate, closeRef } = useDueDate()
+
+    return (
+        <section className=''>
+            <div className='m-1 py-0.5 px-1 space-y-2'>
+                <Select
+                    onValueChange={onChange}
+                >
+                    <SelectTrigger>
+                        <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                        <SelectItem value="0">Today</SelectItem>
+                        <SelectItem value="1">Tomorrow</SelectItem>
+                        <SelectItem value="3">In 3 days</SelectItem>
+                        <SelectItem value="7">In a week</SelectItem>
+                    </SelectContent>
+                </Select>
+                <div className="rounded-md border mt-2">
+                    <Calendar mode="single" selected={date} onSelect={setDate} />
+                </div>
+            </div>
+            <DatesForm date={date} formRef={formRef} handleSubmit={handleSubmit} closeRef={closeRef} />
+        </section>
+    )
+}
