1120667312f1abf713a0ce7cbae4eea59d48c39b
Othamae <vero.gconesa@gmail.com>
2024-02-12 14:51:03 +0100

    feat: ✨ Added DatesButton component

---
+++ components/modals/cardModal/addToCard/DatesButton.tsx
@@ -0,0 +1,39 @@
+'use client'
+import { DatePicker } from '@/components/DatePicker'
+import { Button } from '@/components/ui/button'
+import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { ADD_DUE_DATE, TYPE_DATES } from '@/const/const'
+import { Clock, X } from 'lucide-react'
+
+interface DatesButtonProps {
+    cardId: string
+}
+
+const DatesButton = ({ cardId }: DatesButtonProps) => {
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button
+                    variant='gray'
+                    className='w-full justify-start'
+                    size='inLine'
+                >
+                    <Clock className='w-4 h-4 mr-2' />
+                    {TYPE_DATES}
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className='px-0 pt-3 pb-3' side='bottom' align='start'>
+                <div className='text-sm font-medium text-center text-neutral-600 pb-4'>{ADD_DUE_DATE}</div>
+                <PopoverClose asChild>
+                    <Button className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600' variant='ghost'>
+                        <X className='h-4 w-4' />
+                    </Button>
+                </PopoverClose>
+                <DatePicker />
+            </PopoverContent>
+        </Popover>
+
+    )
+}
+
+export default DatesButton
