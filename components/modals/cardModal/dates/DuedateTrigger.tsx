e2f4af6fafff889b8c6d3bc064d2c9fa1a3e2301
Othamae <vero.gconesa@gmail.com>
2024-02-14 14:45:42 +0100

    fix: 🐛 Fixed checked value for duedateTrigger

--- components/modals/cardModal/dates/DuedateTrigger.tsx
+++ components/modals/cardModal/dates/DuedateTrigger.tsx
@@ -19,7 +19,7 @@ const DuedateTrigger = ({ cardData }: DuedateTriggerProps) => {
         duedate &&
         <div className={cn('w-full justify-start flex h-6 items-center bg-yellow-200 text-secondary-foreground hover:bg-yellow-300 rounded-full group peer',
             isCompleted && 'bg-green-200 hover:bg-green-300')}>
-                <Checkbox id={cardData.id} className='ml-2  data-[state=checked]:bg-green-700 ' onCheckedChange={onCheckedChange} defaultChecked={isCompleted} />
+                <Checkbox id={cardData.id} className='ml-2  data-[state=checked]:bg-green-700 ' onCheckedChange={onCheckedChange} checked={isCompleted} />
             <PopoverTrigger className='w-full flex items-start peer-data-[state=checked]:line-through '>
                 <Button
                     variant='empty'
