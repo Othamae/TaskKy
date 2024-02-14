d6aa69d441b376b2371b643a05d77f8011e39517
Othamae <vero.gconesa@gmail.com>
2024-02-14 09:39:43 +0100

    refactor: ♻️  Added getProgress function in helpers

--- lib/helpers/helpers.ts
+++ lib/helpers/helpers.ts
@@ -1,4 +1,5 @@
 import { QueryClient } from '@tanstack/react-query'
+import { ChecklistWithTasks } from '../types'
 
 export function reorder<T>(list: T[], startIndex: number, endIndex: number) {
     const result = Array.from(list)
@@ -21,4 +22,9 @@ export function revalidateQueries(cardId: string, queryClient: QueryClient) {
     queryClient.invalidateQueries({
         queryKey: ['card-checkList', cardId],
     })
+}
+
+export function getProgress(checklist: ChecklistWithTasks): [number, string] {
+    const progress = checklist.tasks.filter(task => task.completed).length / checklist.tasks.length * 100
+    return [progress < 1 || isNaN(progress) ? 0.00 : progress, isNaN(progress) ? '0% ' : Math.round(progress) + '%']
 }
