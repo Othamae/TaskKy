a7bfbce9f969ee73e014835156d95e03f15cc5a9
Othamae <vero.gconesa@gmail.com>
2024-02-14 12:25:58 +0100

    feat: ✨ Added card as prop to DatesButton

--- components/modals/cardModal/addToCard/DatesButton.tsx
+++ components/modals/cardModal/addToCard/DatesButton.tsx
@@ -1,14 +1,17 @@
 'use client'
 import { Popover } from '@/components/ui/popover'
+import { CardWithListAndChecklist } from '@/lib/types'
 import CalendarContent from '../dates/CalendarContent'
 import DatesTrigger from '../dates/DatesTrigger'
 
-
-const DatesButton = () => {
+interface DatesButtonProps {
+    card: CardWithListAndChecklist
+}
+const DatesButton = ({ card }: DatesButtonProps) => {
     return (
         <Popover  >
             <DatesTrigger />
-            <CalendarContent inAddToCard />
+            <CalendarContent inAddToCard duedate={card.duedate} />
         </Popover>
     )
 }
