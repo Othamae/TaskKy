bb4bb9fa1fcdd860d79ec02a957230b37d2c1a21
Othamae <vero.gconesa@gmail.com>
2024-02-14 15:33:16 +0100

    style: 💄 Update homePage

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,20 +1,15 @@
 import { Button } from '@/components/ui/button'
 import { headingFont } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
-import { Medal } from 'lucide-react'
 import Link from 'next/link'
 
 export default function MarketingPage() {
 	return (
 		<div className='flex items-center justify-center flex-col'>
 			<div className={cn('flex items-center justify-center flex-col', headingFont.className)}>
-				<div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
-					<Medal className='h-6 w6 mr-2' />
-					No 1 task management
-				</div>
-				<h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>TaskKy helps team move</h1>
-				<div className='text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-2 w-fit'>
-					work forward.
+				<h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>TaskKy empowers</h1>
+				<div className='text-3xl md:text-6xl bg-gradient-to-r from-purple-600 to-fuchsia-600 text-white px-4 p-2 rounded-md pb-2 w-fit'>
+					productivity.
 				</div>
 			</div>
 			<div className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto'>
