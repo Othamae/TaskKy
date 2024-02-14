f15bcc4334502b3e647830d3d4ce84cb20655dcb
Othamae <vero.gconesa@gmail.com>
2024-02-14 09:40:38 +0100

    feat: ✨ Added progress in checklistItem

--- components/modals/cardModal/checklist/ChecklistItem.tsx
+++ components/modals/cardModal/checklist/ChecklistItem.tsx
@@ -1,5 +1,6 @@
 import { ChecklistWithTasks } from '@/lib/types'
 import ChecklistHeader from './ChecklistHeader'
+import ChecklistProgress from './ChecklistProgress'
 import Tasks from './task/Tasks'
 
 interface ChecklistItemProps {
@@ -10,6 +11,7 @@ const ChecklistItem = ({ item }: ChecklistItemProps) => {
     return (
         <li>
             <ChecklistHeader checklist={item} />
+            <ChecklistProgress checklist={item} />
             <Tasks checklist={item} />
         </li>
 
